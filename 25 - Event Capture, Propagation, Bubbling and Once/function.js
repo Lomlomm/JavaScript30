086e1b17b6736ef83af4be550f5d0c217679c5dc
vanessa_arellano <vanessaserna60@gmail.com>
2023-01-17 12:18:50 -0600

    Solve event capture, propagation and bubblingp

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/function.js
@@ -0,0 +1,18 @@
+const divs = document.querySelectorAll('div')
+
+function logText(e){
+    console.log(this.classList.value); 
+    //e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {
+    console.log('Click!!')
+}, {
+    once: true
+})
